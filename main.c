5bb1b87ecf7bd0747e085f8ef3776de0746caeea
GuillaumeJ <guillaumejjeanneret@gmail.com>
2020-05-07 09:28:18 +0200

    main FINAL
    main.c/.h au propre -> final

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include "memory_protection.h"
 #include <usbcfg.h>
 #include <main.h>
-#include <chprintf.h>
 #include <motors.h>
 #include <audio/microphone.h>
 #include <proximity.h>
@@ -15,7 +14,6 @@
 #include <fft.h>
 #include <communications.h>
 #include <arm_math.h>
-#include <pi_regulator.h>
 #include <obstacle.h>
 
 static void serial_start(void)
@@ -30,22 +28,6 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer12_start(void){
-    //General Purpose Timer configuration   
-    //timer 12 is a 16 bit timer so we can measure time
-    //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
-        0,
-        0
-    };
-
-    gptStart(&GPTD12, &gpt12cfg);
-    //let the timer count to max value
-    gptStartContinuous(&GPTD12, 0xFFFF);
-}
-
 int main(void)
 {
     halInit();
@@ -56,8 +38,6 @@ int main(void)
     serial_start();
     //starts the USB communication
     usb_start();
-    //starts timer 12
-    timer12_start();
     //inits the motors
     motors_init();
 
